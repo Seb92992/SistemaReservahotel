0cdd7d5b72d4d4055c9b4c09ece559c83ba03f11
Seb92992 <seaguilarm@ucvvirtual.edu.pe>
2023-11-28 21:17:01 -0500

    Initial commit

---
+++ src/Logica/conexion.java
@@ -0,0 +1,37 @@
+
+package Logica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class conexion {
+    public String db="hotel";
+    public String url="jdbc:mysql://localhost:3307/" +db;
+    public String user="root";
+    public String pass="";
+
+    public conexion() {
+    }
+    
+    public Connection conectar(){
+        Connection link=null;
+        
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver");
+            link=DriverManager.getConnection(this.url, this.user, this.pass);
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+            
+        }
+        
+        return link;
+    }
+    
+   
+    
+    
+}
